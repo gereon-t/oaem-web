88a3598cc26ab752ebffa3fdbff7413ee509b223
Gereon Tombrink <tombrink@igg.uni-bonn.de>
2024-02-13 13:18:42 +0100

    Improve API call

--- oaem-frontend/src/components/PlotComponent.js
+++ oaem-frontend/src/components/PlotComponent.js
@@ -6,30 +6,27 @@ import Plot from 'react-plotly.js';
 const ENDPOINT_URL = 'api';
 
 const getPlot = async (position) => {
-    try {
-        const response = await fetch(ENDPOINT_URL + '/plot?' + new URLSearchParams({
-            pos_x: position[0],
-            pos_y: position[1],
-            epsg: 4326,
-        }), {
-            method: 'GET', headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+    const searchParams = new URLSearchParams({
+        pos_x: position[0],
+        pos_y: position[1],
+        epsg: 4326,
+    });
 
-        if (response.status !== 200) {
-            throw new Error('Plotting failed');
+    const requestOptions = { method: 'GET' }
+
+    return fetch(ENDPOINT_URL + '/plot?' + searchParams, requestOptions).then(response => {
+        if (!response.ok) {
+            throw new Error('Reuqest failed with status ' + response.status + ' and message ' + response.statusText);
         }
-
-        const json_data = await response.json();
+        return response.json();
+    }).then(json_data => {
         return json_data;
-
-
-    } catch (error) {
+    }).catch(error => {
         console.error('Failed to plot oaem:', error);
-    }
-};
+        return null;
+    });
 
+};
 
 
 const PlotModal = ({ isOpen, setIsPlotModalOpen, position }) => {
